ae91712eb820e396a8fa4f79ae0bb8161bcc7474
Ally7574 <allysonvictorsouza@gmail.com>
2026-04-24 20:12:35 -0300

    corrigindo teste eleitoral

--- src/br/com/fiap/Apostila7/view/TesteEleitoral.java
+++ src/br/com/fiap/Apostila7/view/TesteEleitoral.java
@@ -5,39 +5,21 @@ import javax.swing.*;
 public class TesteEleitoral {
     public static void main(String[] args) {
 
-        String p1 = JOptionPane.showInputDialog("nome da primeira pessoa: ");
-        int idade1 = Integer.parseInt(JOptionPane.showInputDialog("quantos anos a primeira pessoa tem: "));
+        int eleitoresObrigatorios = 0;
+        for (int i = 1; i < 4; i++) {
 
-        if (idade1 < 16) {
-            JOptionPane.showMessageDialog(null, p1 + " é não eleitor (abaixo de 16 anos)");
-        } else if (idade1 == 16 || idade1 == 17 || idade1 >= 66) {
-            JOptionPane.showMessageDialog(null, p1 + " é eleitor facultativo (16 , 17 anos ou maior que 65 anos)");
-        } else if (idade1 >= 18 || idade1 <= 65) {
-            JOptionPane.showMessageDialog(null, p1 + " é eleitor obrigatório (entre 18 e 65 anos)");
+            int idade = Integer.parseInt(JOptionPane.showInputDialog("Digite a idade do eleitor" + i));
+            if (idade < 16) {
+                JOptionPane.showMessageDialog(null, "não eleitor");
+            } else if (idade >= 18 && idade <= 65) {
+                JOptionPane.showMessageDialog(null,"Eleitor obrigatorio");
+                eleitoresObrigatorios ++;
+            } else
+                JOptionPane.showMessageDialog(null,"Eleitor facultativo");
         }
-
-        String p2 = JOptionPane.showInputDialog("nome da segunda pessoa: ");
-        int idade2 = Integer.parseInt(JOptionPane.showInputDialog("quantos anos a segunda pessoa tem: "));
-
-        if (idade2 < 16) {
-            JOptionPane.showMessageDialog(null, p2 + " é não eleitor (abaixo de 16 anos)");
-        } else if (idade2 == 16 || idade2 == 17 || idade2 >= 66) {
-            JOptionPane.showMessageDialog(null, p2 + " é eleitor facultativo (16 , 17 anos ou maior que 65 anos)");
-        } else if (idade2 >= 18 || idade2 <= 65) {
-            JOptionPane.showMessageDialog(null, p2 +  " é eleitor obrigatório (entre 18 e 65 anos)");
-        }
+        JOptionPane.showMessageDialog(null,"eleitores obrigatorio: " + eleitoresObrigatorios);
 
 
-        String p3 = JOptionPane.showInputDialog("nome da terceira pessoa: ");
-        int idade3 = Integer.parseInt(JOptionPane.showInputDialog("quantos anos a terceira pessoa tem: "));
-
-        if (idade3 < 16) {
-            JOptionPane.showMessageDialog(null, p3 + " é não eleitor (abaixo de 16 anos)");
-        } else if (idade3 == 16 || idade3 == 17 || idade3 >= 66) {
-            JOptionPane.showMessageDialog(null, p3 + " é eleitor facultativo (16 , 17 anos ou maior que 65 anos)");
-        } else if (idade3 >= 18 || idade3 <= 65) {
-            JOptionPane.showMessageDialog(null, p3 +  " é eleitor obrigatório (entre 18 e 65 anos)");
-        }
 
 
 
